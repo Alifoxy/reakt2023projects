a66456148eff72f38275e53e4d4b6b1696d462c1
Alifoxy <nightfoxy666@gmail.com>
2023-01-27 15:48:06 +0300

    hrreyyh

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-// import {Comment} from "./Comment";
+import {Comment} from "./Comment";
 import {getComments} from "../../services/api/getInfo/getCommentsInfo";
-import {useNavigate} from "react-router-dom";
 
 export const Comments = () => {
-    const navigate = useNavigate();
+
     const [comments, setComments] = useState([]);
     useEffect(() => {
         getComments.getAll().then(({data}) => setComments([...data]))
@@ -13,18 +12,7 @@ export const Comments = () => {
 
     return (
         <div>
-            {comments.map(item => {
-                const {postId, id, name} = item;
-                return (
-                    <div key={id} className={'item2'}>
-                        <div>ID: {id}</div>
-                        <div>PostID: {postId}</div>
-                        <div>Comment Name: {name}</div>
-                        <button onClick={() => navigate(postId.toString())}>Get Post</button>
-                    </div>
-                )
-            })}
-            {/*{comments.map(comment=><Comment key={comment.id} comment={comment}/>)}*/}
+            {comments.map(comment=><Comment key={comment.id} comment={comment}/>)}
         </div>
     );
 };
--- src/pages/Comments/CommentsPage.js
+++ src/pages/Comments/CommentsPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Comments} from "../../components/Comments/Comments";
 import '../Add_style.css';
+import { Outlet } from "react-router-dom";
 
 const CommentsPage = () => {
     return (
         <div>
             <h2 className={'name_of_page'}>Comments</h2>
+            <Outlet />
             <Comments/>
+
         </div>
     );
 };
