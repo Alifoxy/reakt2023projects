435d48cddbfb46d9d16c3d3575e562f75f66b64a
Alifoxy <nightfoxy666@gmail.com>
2023-02-02 23:37:47 +0300

    hrreyyh

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import {Posts} from "./components/Posts/Posts";
 const App = () => {
     return (
         <div>
+            <div><h2>Comments</h2></div>
             <Comments/>
             <hr/>
+            <div><h2>Posts</h2></div>
             <Posts/>
         </div>
     );
