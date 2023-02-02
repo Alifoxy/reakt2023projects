d319d156e79019cfaf75af8e92e54ce257888121
Alifoxy <nightfoxy666@gmail.com>
2023-02-02 11:49:41 +0300

    hrreyyh

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -2,10 +2,10 @@ import React from 'react';
 import '../Add_style.css';
 
 export const Post = ({post}) => {
-    const {postId, title, body} = post;
+    const {id, title, body} = post;
     return (
         <div className={'item2'}>
-            <div>PostID: {postId}</div>
+            <div>PostID: {id}</div>
             <div>Title: {title}</div>
             <div>Content: {body}</div>
 
--- src/services/api/endpoints.js
+++ src/services/api/endpoints.js
@@ -3,5 +3,5 @@ export const baseURL = 'https://jsonplaceholder.typicode.com'
 export const urls = {
     users: '/users',
     comments: 'post/1/comments',
-    posts:'/posts/ID',
+    posts:'/posts',
 }
