ee9ddd925de7a1bc9b396eeaeedab82b1169d2df
Alifoxy <nightfoxy666@gmail.com>
2023-02-02 11:20:13 +0300

    hrreyyh

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import {Post} from "./Post";
 import {getPosts} from "../../services/api/getInfo/getPostsInfo";
+import {Comments} from "../Comments/Comments";
 
-export const Posts = ({id}) => {
-
+export const Posts = () => {
+    const [commentId, setCommentId] = useState(null);
     const [posts, setPosts] = useState([]);
     useEffect(() => {
-        getPosts.getAll(id).then(({data}) => setPosts([...data]))
-    }, [id]);
+        getPosts.getAll(commentId).then(({data}) => setPosts([...data]))
+    }, [commentId]);
 
     return (
         <div>
             {posts.map(post=><Post key={post.id} post={post}/>)}
+            <div className><Comments setCommentId={setCommentId}/></div>
+            <div className>{commentId && <Posts commentId={commentId}/>}</div>
         </div>
     );
 };
