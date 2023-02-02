94d6783ebda286dc92f3c52e92815ca22d8d077d
Alifoxy <nightfoxy666@gmail.com>
2023-02-02 11:24:25 +0300

    hrreyyh

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import {Comment} from "./Comment";
 import {getComments} from "../../services/api/getInfo/getCommentsInfo";
+import {Posts} from "../Posts/Posts";
 
 export const Comments = () => {
-
+    const [commentId, setCommentId] = useState(null);
     const [comments, setComments] = useState([]);
     useEffect(() => {
         getComments.getAll().then(({data}) => setComments([...data]))
@@ -13,6 +14,8 @@ export const Comments = () => {
     return (
         <div>
             {comments.map(comment=><Comment key={comment.id} comment={comment}/>)}
+            <div className><Comments setCommentId={setCommentId}/></div>
+            <div className>{commentId && <Posts commentId={commentId}/>}</div>
         </div>
     );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import {Post} from "./Post";
 import {getPosts} from "../../services/api/getInfo/getPostsInfo";
-import {Comments} from "../Comments/Comments";
 
-export const Posts = () => {
-    const [commentId, setCommentId] = useState(null);
+export const Posts = ({commentId}) => {
+
     const [posts, setPosts] = useState([]);
     useEffect(() => {
         getPosts.getAll(commentId).then(({data}) => setPosts([...data]))
@@ -14,8 +13,6 @@ export const Posts = () => {
     return (
         <div>
             {posts.map(post=><Post key={post.id} post={post}/>)}
-            <div className><Comments setCommentId={setCommentId}/></div>
-            <div className>{commentId && <Posts commentId={commentId}/>}</div>
         </div>
     );
 };
