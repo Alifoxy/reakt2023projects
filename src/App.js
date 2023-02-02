31253fb284d60d49c03b427f893c7b1a3f48c534
Alifoxy <nightfoxy666@gmail.com>
2023-02-02 23:34:38 +0300

    hrreyyh

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     return (
         <div>
             <Comments/>
+            <hr/>
             <Posts/>
         </div>
     );
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -8,7 +8,7 @@ class Post extends Component {
     render() {
         const {id, userId, title, body} = this.props.post;
         return (
-            <div>
+            <div className={'item1'}>
                 <div>id: {id}</div>
                 <div>userId: {userId}</div>
                 <div>title: {title}</div>
