0a6940500a2a3f5005b1e522e896d7470d6b95ee
Alifoxy <nightfoxy666@gmail.com>
2023-02-03 19:53:44 +0300

    hrreyyh

--- src/validators/animalValidator.js
+++ src/validators/animalValidator.js
@@ -5,13 +5,13 @@ export const animalValidator = Joi.object({
         'string.pattern.base':'username can only contain letters and must be between 1 and 20 characters',
     }),
     cat_breed:Joi.string().valid('siamese','ragdoll','maine coon','persian','british','bengal','russian blue','scottish fold','tonkinese','siberian','no breed','other').required().messages({
-        'string.pattern.base': 'breed was not found; possible variants: siamese, ragdoll, maine coon, persian, british, bengal, russian blue, scottish fold, tonkinese, siberian, no breed, other'
+        'string.values.base': 'breed was not found, possible variants: siamese, ragdoll, maine coon, persian, british, bengal, russian blue, scottish fold, tonkinese, siberian, no breed, other'
     }),
     dog_name:Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
         'string.pattern.base':'username can only contain letters and must be between 1 and 20 characters',
     }),
     dog_breed:Joi.string().valid('golden retriever','german shepherd','doberman pinscher','poodle','bulldog','beagle','rottweiler','yorkshire terrier','siberian husky','chihuahua','no breed','other').required().messages({
-        'string.pattern.base': 'breed was not found; possible variants: golden retriever, german shepherd, doberman pinscher, poodle, bulldog, beagle, rottweiler, yorkshire terrier, siberian husky, chihuahua, no breed, other'
+        'string.values.base': 'breed was not found, possible variants: golden retriever, german shepherd, doberman pinscher, poodle, bulldog, beagle, rottweiler, yorkshire terrier, siberian husky, chihuahua, no breed, other'
     })
 });
 
