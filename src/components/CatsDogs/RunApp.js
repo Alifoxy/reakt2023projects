20840f6bea5993ec2349364e99a17edaf148d166
Alifoxy <nightfoxy666@gmail.com>
2023-02-03 19:58:05 +0300

    hrreyyh

--- src/components/CatsDogs/RunApp.js
+++ src/components/CatsDogs/RunApp.js
@@ -52,31 +52,31 @@ const RunApp = () => {
         dogInp2.current.value = ''
     };
 
-    const {register, handleSubmit, formState:{errors,isValid}} = useForm({mode: 'all',resolver:joiResolver(animalValidator)});
+    const {register, formState:{errors}} = useForm({mode: 'all',resolver:joiResolver(animalValidator)});
 
 
 
     return (
         <div>
             <div>
-                <form onSubmit={handleSubmit(createCat)}>
+                <form>
                     <input type="text" ref={catInp1} placeholder="name" {...register('cat_name')}/>
                     {errors.cat_name&&<span>{errors.cat_name.message}</span>}
                     <input type="text" ref={catInp2} placeholder="breed" {...register('cat_breed')}/>
                     {errors.cat_breed&&<span>{errors.cat_breed.message}</span>}
-                    <button disabled={!isValid} >Add new cat</button>
+                    <button onClick={createCat}>Add new cat</button>
                     <Cats cats={state.cats} dispatch={dispatch}/>
                 </form>
 
             </div>
 
             <div>
-                <form onSubmit={handleSubmit(createDog)}>
+                <form>
                     <input type="text" ref={dogInp1} placeholder="name" {...register('dog_name')}/>
                     {errors.dog_name&&<span>{errors.dog_name.message}</span>}
                     <input type="text" ref={dogInp2} placeholder="breed" {...register('dog_breed')}/>
                     {errors.dog_breed&&<span>{errors.dog_breed.message}</span>}
-                    <button disabled={!isValid}>Add new dog</button>
+                    <button onClick={createDog}>Add new dog</button>
                     <Dogs dogs={state.dogs} dispatch={dispatch}/>
                 </form>
             </div>
