8f78fb40e0bab9d0d7c5c027969438db1e0bfe56
Alifoxy <nightfoxy666@gmail.com>
2023-02-03 16:43:02 +0300

    hrreyyh

--- src/validators/catValidator.js
+++ src/validators/catValidator.js
@@ -4,7 +4,7 @@ export const catValidator = Joi.object({
     cat_name:Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
         'string.pattern.base':'username can only contain letters and must be between 1 and 20 characters',
     }),
-    cat_breed:Joi.array().items(Joi.string().valid(['siamese','ragdoll','maine coon','persian','british','bengal','russian blue','scottish fold','tonkinese','siberian','no breed','other'])).required().messages({
+    cat_breed:Joi.string().equal('siamese','ragdoll','maine coon','persian','british','bengal','russian blue','scottish fold','tonkinese','siberian','no breed','other').required().messages({
         'string.pattern.base': 'breed was not found; possible variants: siamese, ragdoll, maine coon, persian, british, bengal, russian blue, scottish fold, tonkinese, siberian, no breed, other'
     })
 });
