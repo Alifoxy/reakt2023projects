bbcabb66fadd4fd382b4eba10d51e6effed7e0e3
Alifoxy <nightfoxy666@gmail.com>
2023-02-03 18:52:34 +0300

    hrreyyh

--- src/components/CatsDogs/RunApp.js
+++ src/components/CatsDogs/RunApp.js
@@ -56,7 +56,7 @@ const RunApp = () => {
     return (
         <div>
             <div>
-                <form onClick={createCat}>
+                <form onInput={createCat}>
                     <input type="text" ref={catInp1} placeholder="name"/>
                     {errors.cat_name&&<span>{errors.cat_name.message}</span>}
                     <input type="text" ref={catInp2} placeholder="breed"/>
@@ -68,7 +68,7 @@ const RunApp = () => {
             </div>
 
             <div>
-                <form onClick={createDog}>
+                <form onSubmit={createDog}>
                     <input type="text" ref={dogInp1} placeholder="name"/>
                     {errors.dog_name&&<span>{errors.dog_name.message}</span>}
                     <input type="text" ref={dogInp2} placeholder="breed"/>
