443abb36ec3b91e40fba8e394856c53b67f68405
Alifoxy <nightfoxy666@gmail.com>
2023-02-09 17:09:58 +0300

    new commit

--- src/validators/carValidator.js
+++ src/validators/carValidator.js
@@ -1,7 +1,9 @@
 import Joi from "joi";
 
 export const carValidator = Joi.object({
-        brand: Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required(),
+        brand: Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
+                'string.pattern.base': 'username can only contain letters and must be between 1 and 20 characters',
+        })
         price: Joi.number().min(0).max(1000000).required(),
         year: Joi.number().min(1990).max(new Date().getFullYear()).required()
 });
