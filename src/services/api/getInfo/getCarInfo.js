c5ca24b6c034cd0c2c1e27c7722b025d218d37db
Alifoxy <nightfoxy666@gmail.com>
2023-02-09 11:37:14 +0300

    new commit

--- src/services/api/getInfo/getCarInfo.js
+++ src/services/api/getInfo/getCarInfo.js
@@ -1,2 +1,8 @@
 import {baseURL} from "../endpoints";
 import {urls} from "../endpoints";
+import {axiosRequest} from "../../axios/axiosConfig";
+
+export const getPosts = {
+    getAll: () => axiosRequest.get(urls.cars),
+    getById: (id) => axiosRequest.get(`${urls.cars}/${id}`),
+}
