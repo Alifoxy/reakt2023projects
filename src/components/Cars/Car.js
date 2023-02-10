3e82c7425c7e95d9d8131d5ff1561535a6fad802
Alifoxy <nightfoxy666@gmail.com>
2023-02-10 13:44:30 +0300

    new commit

--- src/components/Cars/Car.js
+++ src/components/Cars/Car.js
@@ -4,16 +4,19 @@ import '../Add_style.css';
 import {carActions} from "../../redux/slices/car_slice";
 
 const Car = ({car}) => {
+
     const {id, brand, price, year} = car;
+
     const dispatch = useDispatch();
+
     return (
         <div className={'item1'}>
             <div>id: {id}</div>
             <div>brand: {brand}</div>
             <div>price: {price}</div>
             <div>year: {year}</div>
-            <button onClick={()=>dispatch(carActions.set_updateCar(car))}>select</button>
-            <button onClick={()=>dispatch(carActions.deleteById({id}))}>api select</button>
+            <button onClick={()=>dispatch(carActions.set_updateCar(car))}>update</button>
+            <button onClick={()=>dispatch(carActions.deleteById({id}))}>delete</button>
         </div>
     );
 };
