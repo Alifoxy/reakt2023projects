62c7c4123c6a7f8efb02ffec29881b488c093612
Alifoxy <nightfoxy666@gmail.com>
2023-02-14 12:22:04 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 import {useSelector} from "react-redux";
 
 import {Cars} from './components/Cars/Cars';
-import {Header} from "./components/Header/Header";
+import {Form} from "./components/Form/Form";
 
 const App = () => {
     const {loading} = useSelector(state => state.cars);
     return (
         <div>
-            <Header/>
+            <Form/>
             <hr/>
             {loading && <h2>Loading, pls wait .............................</h2>}
             <Cars/>
